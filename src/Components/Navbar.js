064aea9ed1ac088c1ed9d9b0885563fba9c4559e
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-05 19:39:12 +0530

    complete NewsWatch_App with API

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <nav className="navbar fixed-top navbar-expand-lg navbar-light p-0 m-0 shadow" style={{backgroundColor: "#FBDA61",
                 backgroundImage: "linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%)"}}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-3 ms-3" to="/">NewsWatch🧾</Link>
+                    <Link className="navbar-brand fs-3 ms-3" to="/">NewsWatch 🧾</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
